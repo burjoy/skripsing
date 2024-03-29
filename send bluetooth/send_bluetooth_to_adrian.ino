b809d9a2cd74963200420fe070f048069f587713
Luqman <51292479+burjoy@users.noreply.github.com>
2024-03-29 16:51:04 +0700

    ganti mekanisme nyalain bluetooth
    Bluetooth sekarang udah bisa nyala berdasarkan ignition key, gk perlu dinyalain di setup duliu

--- send bluetooth/send_bluetooth_to_adrian.ino
+++ send bluetooth/send_bluetooth_to_adrian.ino
@@ -4,6 +4,8 @@
 #include "BluetoothSerial.h"
 #include "TimeLib.h"
 #include <ESP32SPISlave.h>
+#include "driver/twai.h"
+#include <ESP32Time.h>
 
 // Check if Bluetooth configs are enabled
 #if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
@@ -12,6 +14,9 @@
 
 // Bluetooth Serial object
 BluetoothSerial SerialBT;
+
+#define RX_PIN 25 //tar ganti ke mana (jangan 4 dan 5) buat akomodasi rtc ama clk
+#define TX_PIN 33 // tar ganti ke mana (jangan 4 dan 5) buat akomodasi rtc ama miso
 
 tmElements_t my_time;  // time elements structure
 time_t unix_timestamp; // a timestamp
@@ -25,77 +30,145 @@ String waktu_end;
 String waktu_start;
 String permintaan;
 String nama_file;
+String hasil_baca_file;
+String lokasi_file;
+const char* tempat_file;
 
 int first_slice;
 int second_slice;
 int third_slice;
 int fourth_slice;
+long int prevMillis = 0;
+int nyalain_bluetooth = 0;
+int matiin_bluetooth = 0;
 
 char incomingChar;
 int stop = 0;
 bool deviceConnected = false;
 TaskHandle_t spiTaskHandle = NULL;
+const char* data_vcu_now_cstr;
 
 ESP32SPISlave slave;
+ESP32Time rtc(3600);
+
+twai_message_t vcu_message;
+twai_message_t send_to_vcu;
 
 static constexpr uint32_t BUFFER_SIZE {32};
 uint8_t spi_slave_tx_buf[BUFFER_SIZE];
 uint8_t spi_slave_rx_buf[BUFFER_SIZE];
 
-void spiTask(void *pvParameters) {
-  while (1) {
-    processSPI();
-    vTaskDelay(pdMS_TO_TICKS(10)); // Adjust the delay as needed
-  }
-}
-
-void readFile(fs::FS &fs, const char *path) {
-  Serial.printf("Reading file: %s\n", path);
-
-  File file = fs.open(path);
-  if (!file) {
-    Serial.println("Failed to open file for reading");
-    String gagal = "gagal gan";
-    SerialBT.print(gagal);
-    SerialBT.println();
-    delay(10);
+void writeFile(fs::FS &fs, const char * path, const char * message) {
+  Serial.printf("Writing file: %s\n", path);
+  File file = fs.open(path, FILE_WRITE);
+  
+  if(!file) {
+    Serial.println("Failed to open file for writing");
     return;
   }
-
-  Serial.print("Read from file: ");
-  while (file.available()) {
-    SerialBT.print(file.read());
-    SerialBT.println();
+  
+  if(file.print(message)) {
+    Serial.println("File written");
+  } else {
+    Serial.println("Write failed");
   }
+  
   file.close();
 }
 
-void readBluetooth(void *arg) {
+void spiTask(void *pvParameters) {
   while (1) {
-    if (SerialBT.available()) {
-      incomingChar = SerialBT.read();
-      if (incomingChar != '\n') {
-        message += String(incomingChar);
-      } else {
-        message = "";
-      }
-    }
-    SerialBT.print(message);
-    if (message == "baca inverter") {
-      readFile(SD, "/inverter_status.txt");
-      printf("\nPanjang message: %d", message.length());
-    }
-    if (message == "baca pdu") {
-      readFile(SD, "/pdu_status.txt");
-      printf("\nPanjang message: %d", message.length());
-    }
-    if (message == "baca dcdc") {
-      readFile(SD, "/dcdc_state.txt");
-      printf("\nPanjang message: %d", message.length());
-    }
-    message = "";
+    processSPI();
+    vTaskDelay(pdMS_TO_TICKS(1000)); // Adjust the delay as needed
   }
 }
+
+void twai_setup_and_install_for_send(){
+    //Initialize configuration structures using macro initializers
+    twai_general_config_t g_config = TWAI_GENERAL_CONFIG_DEFAULT((gpio_num_t)TX_PIN, (gpio_num_t)RX_PIN, TWAI_MODE_NORMAL);
+    twai_timing_config_t t_config = TWAI_TIMING_CONFIG_250KBITS();
+    twai_filter_config_t f_config = TWAI_FILTER_CONFIG_ACCEPT_ALL();
+
+    // Install TWAI driver for bus 1
+    // g_config.controller_id = 0;
+    if (twai_driver_install(&g_config, &t_config, &f_config) == ESP_OK) {
+        printf("Driver installed\n");
+    } else {
+        printf("Failed to install driver\n");
+        return;
+    }
+
+    // Start TWAI driver
+    if (twai_start() == ESP_OK) {
+        printf("Driver started\n");
+    } else {
+        printf("Failed to start driver\n");
+        return;
+    }
+}
+
+void readFile(fs::FS &fs, long int start_date, long int end_date, String filter) {
+  // Serial.printf("Reading file: %s\n", path);
+
+  if(filter.indexOf("engine") != -1 || filter.indexOf("inverter") != -1 || filter.indexOf("pdu") != -1){
+    nama_file = "_inverterPDUState.txt";
+  }
+  else if(filter.indexOf("dcdc") != -1 || filter.indexOf("cell_voltage") != -1){
+    nama_file = "_dcdcBMSState.txt";
+  }
+  for(long int epoch = start_date;epoch <= end_date;epoch+=86400){
+    lokasi_file = String(epoch) + nama_file;
+    tempat_file = lokasi_file.c_str();
+    File file = fs.open(tempat_file, FILE_READ);
+    if (!file) {
+      Serial.println("Failed to open file for reading");
+      String gagal = "\ngagal gan, file gk ada";
+      SerialBT.print(gagal);
+      SerialBT.println();
+      delay(10);
+      // return;
+    }
+
+    Serial.print("Read from file: ");
+    while (file.available()) {
+      // SerialBT.print(file.read());
+      // SerialBT.println();
+      hasil_baca_file = file.readString();
+      if(hasil_baca_file.indexOf(filter) != -1){
+        SerialBT.print(hasil_baca_file);
+      }
+      delay(10);
+    }
+    file.close();
+  }
+}
+
+// void readBluetooth(void *arg) {
+//   while (1) {
+//     if (SerialBT.available()) {
+//       incomingChar = SerialBT.read();
+//       if (incomingChar != '\n') {
+//         message += String(incomingChar);
+//       } else {
+//         message = "";
+//       }
+//     }
+//     SerialBT.print(message);
+//     if (message == "baca inverter") {
+//       readFile(SD, "/inverter_status.txt");
+//       printf("\nPanjang message: %d", message.length());
+//     }
+//     if (message == "baca pdu") {
+//       readFile(SD, "/pdu_status.txt");
+//       printf("\nPanjang message: %d", message.length());
+//     }
+//     if (message == "baca dcdc") {
+//       readFile(SD, "/dcdc_state.txt");
+//       printf("\nPanjang message: %d", message.length());
+//     }
+//     message = "";
+//   }
+// }
 
 void read_request() {
     // processSPI();
@@ -142,12 +215,16 @@ void read_request() {
           long int waktu_mulai = change_to_unix(year_start, month_start, day_start);
           long int waktu_akhir = change_to_unix(year_end, month_end, day_end);
 
-          if(data == 1){
-            process_request(waktu_mulai, waktu_akhir, request);
-          }
-          else{
-            SerialBT.print("\nAkses tidak tersedia");
-          }
+          //komen ama unkomen 2 process request dibawah ini buat demo & testing
+
+          process_request(waktu_mulai, waktu_akhir, request);
+
+          // if(data == 1){
+          //   process_request(waktu_mulai, waktu_akhir, request);
+          // }
+          // else{
+          //   SerialBT.print("\nAkses tidak tersedia");
+          // }
           // SerialBT.print(waktu_mulai);
           // SerialBT.print(datetime_akhir);
           // SerialBT.print(request);
@@ -156,6 +233,9 @@ void read_request() {
           // datetime_awal = "";
           // datetime_akhir = "";
           // request = "";
+          printf("\nPesen: %s", message);
+          // SerialBT.end();
+          // printf("\ncoba liat mati apa kagak");
           message = "";
         }
       }
@@ -212,22 +292,28 @@ void process_request(long int waktu_mulai, long int waktu_akhir, String request)
 
   switch(kiriman){
     case 1:
-      SerialBT.print("\nDashboard");
+      SerialBT.print("\nTegangan Inverter");
+      readFile(SD, waktu_mulai, waktu_akhir, "inverter_voltage");
       break;
     case 2:
-      SerialBT.print("\nSpeeds");
+      SerialBT.print("\nSpeed");
+      readFile(SD, waktu_mulai, waktu_akhir, "engine_speed");
       break;
     case 3:
       SerialBT.print("\nRPM");
+      readFile(SD, waktu_mulai, waktu_akhir, "inverter_rpm");
       break;
     case 4:
       SerialBT.print("\nTemperature");
+      readFile(SD, waktu_mulai, waktu_akhir, "engine_temp");
       break;
     case 5:
-      SerialBT.print("\nBattery temperature");
+      SerialBT.print("\nDCDC Current");
+      readFile(SD, waktu_mulai, waktu_akhir, "dcdc_current");
       break;
     case 6:
-      SerialBT.print("\nCell battery");
+      SerialBT.print("\nCell battery voltage");
+      readFile(SD, waktu_mulai, waktu_akhir, "cell_voltage");
       break;
     // default:
     //   SerialBT.print("\nInvalid Code Request");
@@ -238,6 +324,40 @@ void process_request(long int waktu_mulai, long int waktu_akhir, String request)
   waktu_end = "";
   permintaan = "";
 }
+
+void getFromVCU(void *arg){
+  while(1){
+    long int currentMillis = millis();
+    if(twai_receive(&vcu_message, pdMS_TO_TICKS(1000)) == ESP_OK){
+      if(currentMillis - prevMillis >= 8000){
+        if(data == 0){
+          printf("\nDapetin data dari vcu lu anjeng");
+        }
+        if(data == 1){
+          printf("\nJangan ambil data dulu");
+        }
+        prevMillis = currentMillis;
+      }
+    }
+    else{
+      //ini buat contoh aja, tar ilangin lagi
+      if(currentMillis - prevMillis >= 8000){
+        if(data == 0){
+          printf("\nData gk dapet tai");
+        }
+        if(data == 1){
+          printf("\nGw bilang kgk dapet data");
+        }
+        prevMillis = currentMillis;
+      }      
+    }
+    vTaskDelay(100);
+  }
+}
+
+// void write_to_sd(){
+
+// }
 
 void processSPI(){
   slave.wait(spi_slave_rx_buf, spi_slave_tx_buf, BUFFER_SIZE);
@@ -250,18 +370,105 @@ void processSPI(){
   if(data == 1 )
   {
     Serial.println("Setting LED active HIGH ");
+    if(nyalain_bluetooth != 1){
+      SerialBT.begin("ESP32-Slave");
+      nyalain_bluetooth = 1;
+      matiin_bluetooth = 0;
+    }
     // digitalWrite(15, HIGH);
   }
   else if(data == 0 )
   {
     Serial.println("Setting LED active LOW ");
+    if(matiin_bluetooth != 1){
+      SerialBT.end();
+      nyalain_bluetooth = 0;
+      matiin_bluetooth = 1;
+    }
+    //inget, serialbt.end buat end bluetooth
   }
+  else if(data == 5){
+    write_data();
+  }
+}
+
+void write_data(){
+  Serial.println("Checked program");
 }
 
 //inget, epoch +1 hari itu +86400
 
+void decodeData(unsigned long combined_data) {
+  int year = (combined_data >> 48) & 0xFFFF; // Extract year (bits 48-63)
+  int month = (combined_data >> 40) & 0xFF;   // Extract month (bits 40-47)
+  int day = (combined_data >> 32) & 0xFF;     // Extract day (bits 32-39)
+  int hour = (combined_data >> 24) & 0xFF;    // Extract hour (bits 24-31)
+  int minute = (combined_data >> 16) & 0xFF;  // Extract minute (bits 16-23)
+  int second = (combined_data >> 8) & 0xFF;   // Extract second (bits 8-15)
+
+  // Do something with the decoded data, such as printing it
+  Serial.print("Received Date and Time: ");
+  Serial.print(year);
+  Serial.print("-");
+  Serial.print(month);
+  Serial.print("-");
+  Serial.print(day);
+  Serial.print(" ");
+  Serial.print(hour);
+  Serial.print(":");
+  Serial.print(minute);
+  Serial.print(":");
+  Serial.println(second);
+}
+
 void setup() {
+  byte datetimeBytes[7];
+  slave.setDataMode(SPI_MODE0);
+  slave.begin();
+
+  memset(spi_slave_tx_buf, 0, BUFFER_SIZE);
+  memset(spi_slave_rx_buf, 0, BUFFER_SIZE);
+
+  slave.wait(spi_slave_rx_buf, spi_slave_tx_buf, BUFFER_SIZE);
+
+  // Decode received data
+  unsigned long combined_data;
+  for (int i = 0; i < BUFFER_SIZE; i++) {
+    // combined_data |= (unsigned long)spi_slave_rx_buf[i] << (8 * (BUFFER_SIZE - 1 - i));
+    datetimeBytes[i] = spi_slave_rx_buf[i];
+  }
+  // Serial.println(combined_data);
+  // decodeData(combined_data);
+  Serial.println(datetimeBytes[2]);
+
+  slave.pop(); // Clear the buffer
+
+  int year = (datetimeBytes[0] << 8) | datetimeBytes[1];
+  byte month = datetimeBytes[2];
+  byte day = datetimeBytes[3];
+  byte hour = datetimeBytes[4];
+  byte minute = datetimeBytes[5];
+  byte second = datetimeBytes[6];
+
+  Serial.println(year);
+  Serial.println(hour);
+
+  long int unix_now = change_to_unix(year, int(month), int(day));
+  String data_vcu_now = "/" + String(unix_now) + "_vcuNow.txt";
+  data_vcu_now_cstr = data_vcu_now.c_str();
+
+  pinMode(32, OUTPUT);
+  pinMode(27, OUTPUT);
+  pinMode(26, OUTPUT);
+  pinMode(4, OUTPUT);
+
+  digitalWrite(32, HIGH);
+  digitalWrite(26, HIGH);
+  digitalWrite(27, HIGH);
+  digitalWrite(4, HIGH);
+  
   Serial.begin(115200);
+
   if (!SD.begin(5)) {
     Serial.println("Card Mount Failed");
   }
@@ -285,20 +492,32 @@ void setup() {
   uint64_t cardSize = SD.cardSize() / (1024 * 1024);
   Serial.printf("SD Card Size: %lluMB\n", cardSize);
 
-  slave.setDataMode(SPI_MODE0);
-  slave.begin();
+  File file = SD.open(data_vcu_now_cstr, FILE_APPEND);
+  if(!file){
+    Serial.println("File vcu belum ada");
+    writeFile(SD, data_vcu_now_cstr, "\n");
+  }
+  else{
+    Serial.println("Data vcu filenya sudah ada");
+  }
 
-  memset(spi_slave_tx_buf, 0, BUFFER_SIZE);
-  memset(spi_slave_rx_buf, 0, BUFFER_SIZE);
+  digitalWrite(32, LOW);
+  digitalWrite(26, LOW);
+  digitalWrite(27, LOW);
+  digitalWrite(4, LOW);
+
+  // delay(200);  
+
+  twai_setup_and_install_for_send();
+  // SerialBT.begin("ESP32-Slave");
 
   xTaskCreatePinnedToCore(spiTask, "spiTask", 3000, NULL, 2, &spiTaskHandle, 1);
-
-  SerialBT.begin("ESP32-Slave");
+  xTaskCreatePinnedToCore(getFromVCU, "get_from_vcu", 3000, NULL, 3, NULL, 1);
 
   Serial.println("The device started, now you can pair it with Bluetooth!");
 
   // xTaskCreatePinnedToCore(read_request, "read_request", 3000, NULL, 2, NULL, 0);
-  // xTaskCreatePinnedToCore(processSPI, "processSPI", 3000, NULL, 2, NULL, 1);
+  // xTaskCreatePinnedToCore(processSPI, "", 3000, NULL, 2, NULL, 1);
   // vTaskDelete(NULL);
 }
 
