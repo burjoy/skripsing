e25aef661db2b65c2ca03a0732ad7875ebec5f28
Luqman <51292479+burjoy@users.noreply.github.com>
2024-05-06 21:47:07 +0700

    added self check mechanism

--- send bluetooth/send_bluetooth_to_adrian.ino
+++ send bluetooth/send_bluetooth_to_adrian.ino
@@ -6,6 +6,10 @@
 #include <ESP32SPISlave.h>
 #include "driver/twai.h"
 #include <ESP32Time.h>
+#include <uECC.h>
+#include "mbedtls/base64.h"
+#include "esp_random.h"
+#include <time.h>
 
 // Check if Bluetooth configs are enabled
 #if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
@@ -46,24 +50,255 @@ int tahun_lalu;
 
 int log_enable = 0;
 int dtc_enable = 0;
+int check_enable = 0;
 
 char incomingChar;
 int stop = 0;
 bool deviceConnected = false;
 TaskHandle_t spiTaskHandle = NULL;
 const char* data_vcu_now_cstr;
+String waksiu;
+
+String sd_card_status;
+int allowed = 1;
 
 ESP32SPISlave slave;
 // ESP32Time rtc(3600);
 
 twai_message_t vcu_message;
-twai_message_t send_to_vcu;
+// twai_message_t send_to_vcu;
+twai_message_t vcu_message_2;
+twai_message_t vcu_message_3;
+twai_message_t vcu_message_4;
 
 static constexpr uint32_t BUFFER_SIZE {32};
 uint8_t spi_slave_tx_buf[BUFFER_SIZE];
 uint8_t spi_slave_rx_buf[BUFFER_SIZE];
 
+#define SECRET_ARRAY_SIZE 20
+#define PRIV_ARRAY_SIZE 21
+#define PUB_ARRAY_SIZE 40
+
+char dataBuffer[255];
+char result[255];
+
+char otherPubKey[41] = {0};
+
+//char thisPubKey[PUB_ARRAY_SIZE];
+unsigned char thisPubKey[41];
+char thisPrivKey[PRIV_ARRAY_SIZE];
+char thisSharedSecret[SECRET_ARRAY_SIZE];
+char otherSharedSecret[SECRET_ARRAY_SIZE];
+const struct uECC_Curve_t * curve = uECC_secp160r1();
+
 ESP32Time rtc(0);
+
+int randomNumberGenerator(uint8_t *dest, unsigned size_) {
+  esp_fill_random(dest, size_);
+  return 1;
+}
+
+void generatePublicKey() {
+  uECC_make_key((uint8_t*)thisPubKey, (uint8_t*)thisPrivKey, curve);
+  Serial.print("Key pair successfully made!\n"); 
+}
+
+// utility function
+int strStartsWith(const char *a, const char *b) {
+  if (strncmp(a, b, strlen(b)) == 0) return 1;
+  return 0;
+}
+
+// this function is necessary
+// micro-ecc does not introduce 0x04 prefix in the uncompressed public key
+void insertByte(unsigned char *array, size_t *size, int position, unsigned char byte) {
+    // Check if position is valid
+    if (position < 0 || position > *size) {
+        printf("Invalid position\n");
+        return;
+    }
+
+    // Increase size of array
+    (*size)++;
+
+    // Shift elements to the right starting from the end
+    for (int i = *size - 1; i > position; i--) {
+        array[i] = array[i - 1];
+    }
+
+    // Insert the new byte
+    array[position] = byte;
+}
+
+void authentication(){
+   int i = 0;
+   while(SerialBT.available()) {
+    dataBuffer[i] += (char)SerialBT.read();
+    i++;
+  }
+
+  // Process retrieved data
+  if (strlen(dataBuffer) > 0) {
+    // These lines are just for checking
+    Serial.printf("Received data: ");
+    Serial.println(dataBuffer);
+    Serial.printf("Sent data: ");
+
+    // Step/case 1: Initiation
+    if (!strcmp(dataBuffer, "AUTHENTICATION\r\n")) {
+      // Create own pub/priv key
+      uECC_make_key((uint8_t *)thisPubKey, (uint8_t*)thisPrivKey, curve);
+      size_t keySize = 40;
+      insertByte(thisPubKey, &keySize, 0, 0x04);
+
+      // Send pub key
+      unsigned char encodedPublicKey[255] = {0};
+      size_t olen;
+      mbedtls_base64_encode(encodedPublicKey, 255, &olen, thisPubKey, 41);
+      
+      strcpy(result, (String("-----BEGIN PUBLIC KEY-----\n") + String((char*)encodedPublicKey) + String("\n-----END PUBLIC KEY-----")).c_str());
+    }
+
+    // Step/case 2: Parse other public key, calculate shared secret, and prompt desktop to send shared secret
+    else if (strStartsWith(dataBuffer, "-----BEGIN PUBLIC KEY-----")) {
+      // Parsing and decoding logic
+      char *start_, *end_;
+      int length_;
+  
+      // Find the start of the substring
+      start_ = strstr(dataBuffer, "-----BEGIN PUBLIC KEY-----\n");
+      if (start_ == NULL) {
+          Serial.printf("Header not found\n");
+      }
+      start_ += strlen("-----BEGIN PUBLIC KEY-----\n");
+  
+      // Find the end of the substring
+      end_ = strstr(start_, "\n-----END PUBLIC KEY-----");
+      if (end_ == NULL) {
+          Serial.printf("Footer not found\n");
+      }
+
+      // Calculate the length of the substring
+      length_ = end_ - start_;
+  
+      // Allocate memory for the substring
+      unsigned char tempBuffer[length_ + 1] = {0};
+  
+      // Copy the substring into the new buffer
+      memcpy(tempBuffer, start_, length_);
+      tempBuffer[length_] = '\0'; // Null-terminate the string
+      
+      Serial.print("Data buffer (before removal): ");
+      for (int i = 0; i < 127; i++) {
+        Serial.printf("%02X ", dataBuffer[i]);
+      }
+      Serial.println();
+      size_t outlen;
+      mbedtls_base64_decode((unsigned char*)otherPubKey, 41, &outlen, tempBuffer, strlen((char*)tempBuffer));
+      
+      Serial.print("Detected len: "); Serial.println(strlen((char*)tempBuffer));
+      
+      Serial.print("Data buffer (after removal): ");
+      for (int i = 0; i < length_; i++) {
+        Serial.printf("%02X ", tempBuffer[i]);
+      }
+      Serial.println();
+      
+      Serial.print("Decoded public key: ");
+      for (int i = 0; i < PUB_ARRAY_SIZE; i++) {
+        Serial.printf("%02X ", otherPubKey[i]);
+      }
+      Serial.println();
+      Serial.print("Decoded key size: "); Serial.println(outlen);
+
+      // Check public key validity
+      if (!uECC_valid_public_key((uint8_t*)(otherPubKey + 1), curve)) {
+        Serial.println("Other public key not valid!");
+      }
+
+      
+      // and create shared secret from the public key
+      // uint8_t sharedSecret[SECRET_ARRAY_SIZE] = {0};
+      if(!uECC_shared_secret((uint8_t*)(otherPubKey + 1), (uint8_t*)thisPrivKey, (uint8_t*)thisSharedSecret, curve)) {
+        Serial.println("Does not agree!");
+      }
+      Serial.print("Calculated sharedSecret: ");
+      for (int i = 0; i < SECRET_ARRAY_SIZE; i++) {
+        Serial.printf("%02X ", thisSharedSecret[i]);
+      }
+      Serial.println();
+
+      strcpy(result, "AWAITING RESULT");
+    }
+
+    // Step/case 2: Parse other public key, calculate shared secret, and prompt desktop to send shared secret
+    else if (strStartsWith(dataBuffer, "-----BEGIN CERTIFICATE-----")) {
+      // Parsing and decoding logic
+      char *start_, *end_;
+      int length_;
+  
+      // Find the start of the substring
+      start_ = strstr(dataBuffer, "-----BEGIN CERTIFICATE-----\n");
+      if (start_ == NULL) {
+          Serial.printf("Header not found\n");
+      }
+      start_ += strlen("-----BEGIN CERTIFICATE-----\n");
+  
+      // Find the end of the substring
+      end_ = strstr(start_, "\n-----END CERTIFICATE-----");
+      if (end_ == NULL) {
+          Serial.printf("Footer not found\n");
+      }
+
+      // Calculate the length of the substring
+      length_ = end_ - start_;
+  
+      // Allocate memory for the substring
+      unsigned char tempBuffer[length_ + 1] = {0};
+  
+      // Copy the substring into the new buffer
+      memcpy(tempBuffer, start_, length_);
+      tempBuffer[length_] = '\0'; // Null-terminate the string
+      
+      size_t outlen;
+      mbedtls_base64_decode((unsigned char*)otherSharedSecret, SECRET_ARRAY_SIZE, &outlen, tempBuffer, strlen((char*)tempBuffer));
+      Serial.print("Detected len: "); Serial.println(outlen);
+
+      Serial.print("Other shared secret: ");
+      for (int i = 0; i < outlen; i++) {
+        Serial.printf("%02X ", otherSharedSecret[i]);
+      }
+      Serial.println();
+
+      Serial.print("This shared secret: ");
+      for (int i = 0; i < outlen; i++) {
+        Serial.printf("%02X ", thisSharedSecret[i]);
+      }
+      Serial.println();
+
+      if (!memcmp(thisSharedSecret, otherSharedSecret, outlen)) {
+        Serial.println("Shared secret match");
+        allowed = 1;
+        SerialBT.print("ID043102");
+      } else {
+        Serial.println("Shared secret does not match");
+      }
+    }
+  }
+
+  // Send to BT serial
+  if (strcmp(result, "")) {
+    Serial.println("\n--- BT serial ---\n");
+    SerialBT.print(result);
+    Serial.println(result); 
+  }
+
+  // Clear all buffer-related char[]
+  for (int j = 0; j < 255; j++) {
+    dataBuffer[j] = 0;
+    result[j] = 0;
+  }
+}
 
 void writeFile(fs::FS &fs, const char * path, const char * message) {
   Serial.printf("Writing file: %s\n", path);
@@ -233,9 +468,107 @@ void readFile(fs::FS &fs, long int start_date, long int end_date, String filter)
 //   }
 // }
 
+void read_self_check(fs::FS &fs){
+  int waktu_now = change_to_unix(rtc.getYear(), rtc.getMonth(), rtc.getDay());
+  const char* tempat_file;
+  // digitalWrite(32, HIGH);
+  // digitalWrite(26, HIGH);
+  // digitalWrite(27, HIGH);
+  // digitalWrite(4, HIGH);
+  String test_read;
+  // writeFile(SD, "/Test_ajg.txt", "AJGGGG\n");
+  // writeFile(SD, "/Test_kedua.txt", "\n");
+  // File file = fs.open("/Test_ajg.txt", FILE_READ);
+  // test_read = file.readString();
+  // SerialBT.print(test_read);
+
+  // file = fs.open("/Test_kedua.txt");
+  // test_read = file.readString();
+  // SerialBT.print(test_read);
+
+  // if(filter.indexOf("engine") != -1 || filter.indexOf("inverter") != -1 || filter.indexOf("pdu") != -1){
+  //   nama_file = "_inverterPDUState.txt";
+  // }
+  // else if(filter.indexOf("dcdc") != -1 || filter.indexOf("cell_voltage") != -1){
+  //   nama_file = "_dcdcBMSState.txt";
+  // }
+  String nama_file = "dtc_all_now.txt";
+  String filter = "";
+  // String lokasi_file;
+  for(long int epoch = waktu_now;epoch <= waktu_now;epoch+=86400){
+    lokasi_file = String("/") + String(epoch) + String(nama_file);
+    // SerialBT.println(lokasi_file);
+    tempat_file = lokasi_file.c_str();
+    File file = fs.open(tempat_file, FILE_READ);
+    // File file = fs.open("/1712361600_inverterPDUState.txt", FILE_READ);
+    Serial.println("bool Hasil baca file");
+    Serial.print(file);
+    if (!file) {
+      Serial.println("Failed to open file for reading");
+      String gagal = "\ngagal gan, file gk ada";
+      // SerialBT.print(gagal);
+      // SerialBT.println();
+      delay(10);
+      // return;
+    }
+
+    Serial.print("Read from file: ");
+    if(file){
+      while(file.available()) {
+        // file.read();
+        hasil_baca_file = file.readStringUntil('\n');
+        delay(2);
+        // Serial.println(hasil_baca_file);
+        // SerialBT.println(hasil_baca_file);
+        // SerialBT.println();
+        // hasil_baca_file = file.readString();
+        if(hasil_baca_file.indexOf(filter) != -1){
+          hasil_baca_file.replace(filter, "");
+          hasil_baca_file.replace("\t\t", "");
+          hasil_baca_file = hasil_baca_file + "\n";          
+          SerialBT.print(hasil_baca_file);
+          hasil_baca_file = "";
+          SerialBT.flush();
+          Serial.println("Data Sent!");
+          delay(5);          
+        }
+        else{
+          Serial.println("Gk cocok ini string, ey");
+          // SerialBT.println("Gk cocok bang, udah bang");
+          delay(10);
+        }
+        // hasil_baca_file = "";
+        // SerialBT.flush();
+        // delay(500);
+        // break;
+      }
+      // if(!file.available()){
+      //   SerialBT.println("Datanya gk ada bloug");
+      // }
+    }
+    // lokasi_file = "";
+    // tempat_file = lokasi_file.c_str();
+    // tempat_file = '';
+    // file.close();
+    delay(1000);
+  }
+  SerialBT.print("Success!");
+  SerialBT.print("\n");
+  SerialBT.print("\n");
+  // slave.begin();
+  // digitalWrite(32, LOW);
+  // digitalWrite(26, LOW);
+  // digitalWrite(27, LOW);
+  // digitalWrite(4, LOW);
+}
+
 void read_request() {
     // processSPI();
-    if (SerialBT.available()) {
+  if(allowed == 0){  
+    authentication();
+  }
+  if(allowed == 1){
+    while(SerialBT.available()) {
       incomingChar = SerialBT.read();
       // if(isSpace(incomingChar)){
       //   incomingChar = ',';
@@ -249,7 +582,7 @@ void read_request() {
        else {
         // Process and send data only if there is a valid input
         if (!message.isEmpty()) {
-          printf("\n %s", message);
+          printf("Pesen bang:\n %s", message);
           // if(message.equals("LOG,DATA,")){
           //   SerialBT.println("YES");
           // }
@@ -266,12 +599,16 @@ void read_request() {
             log_enable = 1;
             SerialBT.println("LOG READY");
             printf("\nNilai enable: %d", log_enable);                    
-          }
+            }
           // if(message.equals("DTC,RETRIEVAL,")){
           //   dtc_enable = 1;
           //   SerialBT.println("DTC READY");
           //   // send_dtc();
           // }
+          if(message.equals("SELF,CHECK,")){
+            check_enable = 1;
+            // read_self_check();
+          }
           if(log_enable == 1){
             first_slice = message.indexOf(',');
             datetime_awal = message.substring(0, first_slice);
@@ -327,6 +664,7 @@ void read_request() {
         }
       }
     }
+  }
 }
 
 long int change_to_unix(int tahun, int bulan, int hari){
@@ -415,30 +753,51 @@ void process_request(long int waktu_mulai, long int waktu_akhir, String request)
 void getFromVCU(void *arg){
   while(1){
     long int currentMillis = millis();
-    if(twai_receive(&vcu_message, pdMS_TO_TICKS(1000)) == ESP_OK){
-      if(currentMillis - prevMillis >= 8000){
-        if(data == 0){
-          printf("\nDapetin data dari vcu lu anjeng");
+    if(data == 1){
+      if(twai_receive(&vcu_message, pdMS_TO_TICKS(1000)) == ESP_OK){
+        // if(currentMillis - prevMillis >= 8000){
+        //   if(data == 0){
+        //     printf("\nDapetin data dari vcu lu anjeng");
+        //   }
+        //   if(data == 1){
+        //     printf("\nJangan ambil data dulu");
+        //   }
+        //   prevMillis = currentMillis;
+        // }
+        if(vcu_message.identifier == 0x0CF00400){
+          Serial.println(vcu_message.identifier);
         }
-        if(data == 1){
-          printf("\nJangan ambil data dulu");
+      }
+      if(twai_receive(&vcu_message_2, pdMS_TO_TICKS(1000)) == ESP_OK){
+        if(vcu_message_2.identifier == 0x18FE8D00){
+          Serial.println(vcu_message_2.identifier);
         }
-        prevMillis = currentMillis;
+      }
+      if(twai_receive(&vcu_message_3, pdMS_TO_TICKS(1000)) == ESP_OK){
+        if(vcu_message_3.identifier == 0x18F11400){
+          Serial.println(vcu_message_3.identifier);
+        }
+      }
+      if(twai_receive(&vcu_message_4, pdMS_TO_TICKS(1000)) == ESP_OK){
+        if(vcu_message_4.identifier == 0x10F51400){
+          Serial.println(vcu_message_4.identifier);
+        }
       }
     }
-    else{
-      //ini buat contoh aja, tar ilangin lagi
-      if(currentMillis - prevMillis >= 8000){
-        if(data == 0){
-          printf("\nData gk dapet tai");
-        }
-        if(data == 1){
-          printf("\nGw bilang kgk dapet data");
-        }
-        prevMillis = currentMillis;
-      }      
-    }
-    vTaskDelay(100);
+    //kalo strategi diatas gk work, ganti strategi ke gabungin semua id kedalem 1 twai struct message
+    // else{
+    //   //ini buat contoh aja, tar ilangin lagi
+    //   if(currentMillis - prevMillis >= 8000){
+    //     if(data == 0){
+    //       printf("\nData gk dapet tai");
+    //     }
+    //     if(data == 1){
+    //       printf("\nGw bilang kgk dapet data");
+    //     }
+    //     prevMillis = currentMillis;
+    //   }      
+    // }
+    vTaskDelay(100/portTICK_PERIOD_MS);
   }
 }
 
@@ -516,7 +875,7 @@ void processSPI(){
     // String waktu_sekalian = rtc.getTime("%FT%T");
     // Serial.println(waktu_sekalian);
   }
-  if(data == 0 )
+  if(data != 1)
   {
     Serial.println("Setting LED active LOW ");
     if(!message.isEmpty()){
@@ -528,26 +887,56 @@ void processSPI(){
     write_data();
   }
   if(data == 150){
-    Serial.println("Data'e mblaem");
+    // Serial.println("Data'e mblaem");
+    sd_card_status = "Warning\t" + waksiu + "SD Card Mount Failed or Write File Failed";
+  }
+  if(data == 140){
+    sd_card_status = "Warning\t" + waksiu + "SD Card Mount Works";
   }
 }
 
 void write_data(){
-  // digitalWrite(32, HIGH);
-  // digitalWrite(26, HIGH);
-  // digitalWrite(27, HIGH);
-  // digitalWrite(4, HIGH);
+  digitalWrite(32, HIGH);
+  digitalWrite(26, HIGH);
+  digitalWrite(27, HIGH);
+  digitalWrite(4, HIGH);
   String waktu = take_time();
   Serial.println("Checked program");
   Serial.println(waktu);
-  // delay(500);
-  // digitalWrite(32, LOW);
-  // digitalWrite(26, LOW);
-  // digitalWrite(27, LOW);
-  // digitalWrite(4, LOW);
+  delay(500);
+  if(check_enable == 1){
+    read_self_check(SD);
+  }
+  //this delay is only for testing purposes, delete immediately after the code is complete
+  delay(3000);
+  digitalWrite(32, LOW);
+  digitalWrite(26, LOW);
+  digitalWrite(27, LOW);
+  digitalWrite(4, LOW);
 }
 
 //inget, epoch +1 hari itu +86400
+void onBTConnect(esp_spp_cb_event_t event, esp_spp_cb_param_t *param) {
+  if (event == ESP_SPP_SRV_OPEN_EVT) {
+    Serial.println("Device connected!");
+    // if (!deviceConnected) {
+      // sendTerhubung();
+      // deviceConnected = true;
+    // SerialBT.print("Hello World");
+    // SerialBT.print(sd_card_status);
+    // }
+    uint8_t cardType = SD.cardType();
+
+    // if(cardType == CARD_NONE){
+    //     Serial.println("No SD card attached");
+    //     return;
+  }
+  else if (event == ESP_SPP_CLOSE_EVT) {
+      Serial.println("Device disconnected!");
+      allowed = 0;
+      // deviceConnected = false;
+  }
+}
 
 void decodeData(unsigned long combined_data) {
   int year = (combined_data >> 48) & 0xFFFF; // Extract year (bits 48-63)
@@ -610,7 +999,7 @@ void setup() {
   bulan_lalu = month;
   tahun_lalu = year;
 
-  rtc.setTime(second, minute+2, hour, day, month, year);
+  rtc.setTime(second, minute, hour, day, month, year);
 
   Serial.println(year);
   Serial.println(hour);
@@ -618,13 +1007,13 @@ void setup() {
   long int unix_now = change_to_unix(year, int(month), int(day));
   String data_vcu_now = "/" + String(unix_now) + "_vcuNow.txt";
   data_vcu_now_cstr = data_vcu_now.c_str();
+  
+  Serial.begin(115200);
 
   digitalWrite(32, HIGH);
   digitalWrite(26, HIGH);
   digitalWrite(27, HIGH);
   digitalWrite(4, HIGH);
-  
-  Serial.begin(115200);
 
   if (!SD.begin(5)) {
     Serial.println("Card Mount Failed");
@@ -667,9 +1056,12 @@ void setup() {
   digitalWrite(4, LOW);
 
   // delay(200);  
+  waksiu = rtc.getTime("%FT%T\t");
 
   twai_setup_and_install_for_send();
   SerialBT.begin("ESP32-Slave");
+  uECC_set_rng(&randomNumberGenerator);
+  SerialBT.register_callback(onBTConnect);
 
   xTaskCreatePinnedToCore(spiTask, "spiTask", 3000, NULL, 2, &spiTaskHandle, 1);
   xTaskCreatePinnedToCore(getFromVCU, "get_from_vcu", 3000, NULL, 3, NULL, 1);
